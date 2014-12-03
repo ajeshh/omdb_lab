55d2eb1963cd4d911b22d193fade2ce5352f2d4a
ajeshh <ajeshshah@gmail.com>
2014-12-03 07:36:57 -0800

    minor updates

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.get("/movies/bookmark", function (req, res) {
 	res.render("movies/bookmark");
 } );
 
-app.listen(3000, function () {
-  console.log("Visit localhost:3000");
+app.listen(3001, function () {
+  console.log("Visit localhost:3001");
 });
